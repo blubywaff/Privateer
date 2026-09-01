7524676e09d1ac6a1a11d1075a3a43180e4b7cc9
Alex Beamer <beamer2@llnl.gov>
2026-09-01 12:56:18 -0700

    switch to simpler both lru

--- include/privateer/sigaction_virtual_memory_manager.hpp
+++ include/privateer/sigaction_virtual_memory_manager.hpp
@@ -51,7 +51,7 @@ private:
     // SIGACTION-specific members
     std::list<uint64_t> clean_lru;
     std::list<uint64_t> dirty_lru;
-    std::list<uint64_t> both_lre;
+    std::list<uint64_t> both_lru;
     std::set<uint64_t> stash_set;
     std::set<uint64_t> present_blocks;
     
--- src/privateer/sigaction_virtual_memory_manager.cpp
+++ src/privateer/sigaction_virtual_memory_manager.cpp
@@ -277,10 +277,14 @@ void sigaction_virtual_memory_manager::msync() {
         {
           std::lock_guard<std::mutex> lock(sig_handler_mutex);
           clean_lru.push_front((uint64_t)block_address);
+          both_lru.push_front((uint64_t) block_address);
         }
         // }
       });
     dirty_lru.clear();
+    both_lru.clear();
+    both_lru = clean_lru;
+
 
     SPDLOG_LOGGER_INFO(spdlog::default_logger(), "virtual_memory_manager: msync() - Msync Commit Stashed Blocks");
     std::vector<uint64_t> stash_vector(stash_set.begin(), stash_set.end());
@@ -422,7 +426,9 @@ void sigaction_virtual_memory_manager::handler(int sig, siginfo_t* si, void* ctx
         if (is_write_fault){
           // Move from clean_lru to dirty_lru
           clean_lru.remove((uint64_t) block_address);
+          both_lru.remove((uint64_t) block_address);
           dirty_lru.push_front((uint64_t) block_address);
+          both_lru.push_front((uint64_t) block_address);
           if (stash_set.find(block_address) != stash_set.end()){
             // std::cout << "STASHED TO CLEAN TO DIRTY" << std::endl;
             if (!m_block_storage->unstash_block(block_index)){
@@ -585,9 +591,11 @@ void sigaction_virtual_memory_manager::handler(int sig, siginfo_t* si, void* ctx
         // Update LRUs
         if (is_write_fault){
           dirty_lru.push_front(block_address);
+          both_lru.push_front(block_address);
         }
         else{
           clean_lru.push_front(block_address);
+          both_lru.push_front(block_address);
         }
         present_blocks.insert((uint64_t)block_address);
       }
@@ -643,82 +651,41 @@ void sigaction_virtual_memory_manager::update_metadata(int sub_region_index) {
 }
 
 void sigaction_virtual_memory_manager::evict_if_needed() {
-    void* to_evict = (void*) 8888888888888888888ul;
-    if ((present_blocks.size()*m_block_size) >= m_max_mem_size){
-        SPDLOG_LOGGER_INFO(spdlog::default_logger(), "virtual_memory_manager: evict_if_needed() - Evicting");
+    if ((present_blocks.size()*m_block_size) < m_max_mem_size)
+        return;
 
-        auto lre_idx = both_lre.begin();
-        bool is_dirty = false;
+    SPDLOG_LOGGER_INFO(spdlog::default_logger(), "virtual_memory_manager: evict_if_needed() - Evicting");
 
-        if (both_lre.empty()) {
-            if (!clean_lru.empty()) {
-                to_evict = (void*) clean_lru.back();
-                is_dirty = false;
-            }
-            else {
-                to_evict = (void*) dirty_lru.back();
-                is_dirty = true;
-            }
-            goto done_select;
+    uint64_t to_evict = both_lru.back();
+    both_lru.pop_back();
+    bool is_dirty = to_evict == dirty_lru.back();
+
+    if (is_dirty) {
+        dirty_lru.pop_back();
+        uint64_t block_index = ((uint64_t) to_evict - (uint64_t) m_region_start_address) / m_block_size;
+        SPDLOG_LOGGER_INFO(spdlog::default_logger(), "virtual_memory_manager: evict_if_needed() - Stashing block: {}", block_index);
+        if (!m_block_storage->stash_block((void*) to_evict, block_index)){
+            SPDLOG_LOGGER_ERROR(spdlog::default_logger(), "virtual_memory_manager: Error stashing block with index {}", block_index);
+            exit(-1);
         }
+        stash_set.insert((uint64_t) to_evict);
+    }
+    else {
+        clean_lru.pop_back();
+        SPDLOG_LOGGER_INFO(spdlog::default_logger(), "virtual_memory_manager: evict_if_needed() - Evicting clean block: {}", ((uint64_t) to_evict - (uint64_t) m_region_start_address) / m_block_size);
+    }
 
-        if (!clean_lru.empty()) {
-            to_evict = (void*) clean_lru.back();
-            is_dirty = false;
-            for (auto l_it = lre_idx; l_it != both_lre.end(); ++l_it) {
-                if (*l_it == clean_lru.back()) {
-                    lre_idx = l_it;
-                    goto out_clean;
-                }
-            }
-            goto done_select;
-        }
-out_clean:
-
-        if (!dirty_lru.empty()) {
-            if (clean_lru.empty()) {
-                to_evict = (void*) dirty_lru.back();
-                is_dirty = true;
-            }
-            for (auto l_it = both_lre.begin(); l_it != lre_idx; ++l_it) {
-                if (*l_it == dirty_lru.back())
-                    goto done_select;
-            }
-            to_evict = (void*) dirty_lru.back();
-            is_dirty = true;
-        }
-done_select:
-
-        if (is_dirty) {
-            dirty_lru.pop_back();
-            uint64_t block_index = ((uint64_t) to_evict - (uint64_t) m_region_start_address) / m_block_size;
-            SPDLOG_LOGGER_INFO(spdlog::default_logger(), "virtual_memory_manager: evict_if_needed() - Stashing block: {}", block_index);
-            if (!m_block_storage->stash_block(to_evict, block_index)){
-                SPDLOG_LOGGER_ERROR(spdlog::default_logger(), "virtual_memory_manager: Error stashing block with index {}", block_index);
-                exit(-1);
-            }
-            stash_set.insert((uint64_t) to_evict);
-        }
-        else {
-            SPDLOG_LOGGER_INFO(spdlog::default_logger(), "virtual_memory_manager: evict_if_needed() - Evicting clean block: {}", ((uint64_t) to_evict - (uint64_t) m_region_start_address) / m_block_size);
-            clean_lru.pop_back();
-        }
-
-        both_lre.remove((uint64_t) to_evict);
-        both_lre.push_front((uint64_t) to_evict);
-
-        int protect_status = mprotect(to_evict, m_block_size, PROT_NONE);
-        if (protect_status == -1){
+    int protect_status = mprotect((void*) to_evict, m_block_size, PROT_NONE);
+    if (protect_status == -1) {
         SPDLOG_LOGGER_ERROR(spdlog::default_logger(), "virtual_memory_manager: Error evicting address {}", to_evict);
         exit(-1);
-        }
+    }
 
-        int madvise_status = madvise(to_evict, m_block_size, MADV_DONTNEED);
-        if (madvise_status == -1){
+    int madvise_status = madvise((void*) to_evict, m_block_size, MADV_DONTNEED);
+    if (madvise_status == -1) {
         SPDLOG_LOGGER_ERROR(spdlog::default_logger(), "virtual_memory_manager: Error madvising address {}", to_evict);
         exit(-1);
-        }
-
-        present_blocks.erase((uint64_t) to_evict);
     }
+
+    present_blocks.erase((uint64_t) to_evict);
 }
